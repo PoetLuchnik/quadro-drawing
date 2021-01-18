753ddc98e0e955eb79732ef67e7ca5aa3a2be137
PoetLuchnik <artem.pirunow@gmail.com>
2021-01-18 21:15:30 +0200

    System.Drawing referenced. Empty class Qitmap created

---
+++ QuadroDrawing/Qitmap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace QuadroDrawing
+{
+    /// <summary> Object like Bitmap, but weird encryption and decryption </summary>
+    public class Qitmap
+    {
+        /// <summary> Bytes... </summary>
+        private class Data
+        {
+            // TODO
+        }
+
+        Data data;
+
+        public Qitmap(Bitmap bmp)
+        {
+            // TODO
+        }
+    }
+}
